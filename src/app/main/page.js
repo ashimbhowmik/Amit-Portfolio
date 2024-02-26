505f5d84d429f558e7e9f554e577e9d185126b37
ashimbhowmik <ashimbhowmik239@gmail.com>
2024-02-26 21:58:28 +0600

    [bug fixed4]

--- src/app/main/page.js
+++ src/app/main/page.js
@@ -1,12 +1,48 @@
-import ExtractAllData from "@/components/extractData";
+import ClientAboutView from "@/components/client-view/about";
+import ClientContactView from "@/components/client-view/contact";
+import ClientExperienceView from "@/components/client-view/experience";
+import ClientHomeView from "@/components/client-view/home";
+import ClientProjectView from "@/components/client-view/project";
+import { extractAllDatas } from "@/services";
+
 import React from "react";
 
-const Main = () => {
+export default async function Main() {
+  const homeSectionData = await extractAllDatas("home");
+  const aboutSectionData = await extractAllDatas("about");
+  const experienceSectionData = await extractAllDatas("experience");
+  const educationSectionData = await extractAllDatas("education");
+  const projectSectionData = await extractAllDatas("project");
   return (
     <div>
-      <ExtractAllData></ExtractAllData>
+      <ClientHomeView
+        data={
+          homeSectionData && homeSectionData.length ? homeSectionData[0] : []
+        }
+      ></ClientHomeView>
+      <ClientAboutView
+        data={
+          aboutSectionData && aboutSectionData.length ? aboutSectionData[0] : []
+        }
+      ></ClientAboutView>
+      <ClientProjectView
+        educationData={
+          educationSectionData && educationSectionData
+            ? educationSectionData[0]
+            : []
+        }
+        experienceData={
+          experienceSectionData && experienceSectionData
+            ? experienceSectionData[0]
+            : []
+        }
+      ></ClientProjectView>
+      <ClientExperienceView
+        data={
+          projectSectionData && projectSectionData ? projectSectionData[0] : []
+        }
+      ></ClientExperienceView>
+      <ClientContactView></ClientContactView>
     </div>
   );
-};
-
-export default Main;
+}
